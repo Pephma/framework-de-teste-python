98a65c1a59e19b5a82218160a5b929e92776e71c
Pephma <pephmaalt@gmail.com>
2025-09-30 13:59:38 -0300

    2. Classe TestCase

---
+++ meu_xunit.py
@@ -0,0 +1,15 @@
+class TestCase:
+    def __init__(self, test_method_name):
+        self.test_method_name = test_method_name
+
+    def set_up(self):
+        pass
+
+    def tear_down(self):
+        pass
+
+    def run(self):
+        self.set_up()
+        test_method = getattr(self, self.test_method_name)
+        test_method()
+        self.tear_down()
